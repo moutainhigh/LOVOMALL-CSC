21b3c4a7d2b74a5a69145c6f417087128912a683
NeuropathyCC <neuropathycc@gmail.com>
2019-09-26 13:58:25 +0800

    基础框架搭建，添加web、activemq、数据库驱动、cloud、eruka、lombok等依赖，详细请查看pom文件

---
+++ src/main/java/com/lovomall/csc/LovoMallCSCApplication.java
@@ -0,0 +1,13 @@
+package com.lovomall.csc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovoMallCSCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovoMallCSCApplication.class, args);
+    }
+
+}
